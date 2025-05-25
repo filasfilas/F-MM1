c0a079ab344a730b810215a23f0446b6b5c37fc2
filasfilas <filas@mail.ru>
2025-05-25 13:37:43 +0300

    market code started

--- model/shops/market.cpp
+++ model/shops/market.cpp
@@ -3,10 +3,18 @@
 //#include <fstream>
 //#include <iostream>
 
+Market::Market(GameModel* pGM)
+:_pGameModel (pGM)
+{}
 
-
-
-bool Map::isDarkMap(){
-	return _mapdata[_currentMapId][46]&0x1;
+int Market::getFoodCost(){
+    int townNum = _pGameModel -> getMapId();
+    if ((townNum<0)||(townNum>4)) return 999999;
+	return _FOOD_COST[townNum];
 }
 
+
+void Market::buyFood(){
+    int townNum = _pGameModel -> getMapId();
+    int foodCost = getFoodCost();
+}
--- model/shops/market.h
+++ model/shops/market.h
@@ -6,40 +6,17 @@
 
 class GameModel;
 
-const int FOOD_COST[5]={5, 10, 20, 200, 50};
 
-class Map
+class Market
 {
 	public:
-		Map();
-        void init (GameModel* gm);
-		void select (int id);
-		std::vector<unsigned int> getWalls() const;
-		std::string getPassage(int posX, int posY, DIRECTION dir);
-		int getCurrentId () const {return _currentMapId;};
-		void goNextMap (DIRECTION dir);
+		Market(GameModel* pGM);
 
-		bool isNonMagic(int posX, int posY);
-		bool isDangerous(int posX, int posY);
-		bool isDarkCell(int posX, int posY);
-		bool isSpecial(int posX, int posY);
-		void clearSpecial(int posX, int posY);
-		bool isDarkMap();
-		int	 getEncounterRand();
-
-        void doScript(int posX, int posY, DIRECTION dir);
-
-		void reset();
-
+        int getFoodCost();
+        void buyFood();
 
 	private:
-		int _currentMapId;
-		Maze* _currentMaze;
-		std::vector<unsigned int> _walls;	
-		std::vector<unsigned int> _passage;
-		std::vector<unsigned int> _regionWalls;
-		std::vector<unsigned int> _regionPassage;
-		std::vector<Maze*> _mazelist;
+        GameModel*  _pGameModel;
+        const int FOOD_COST[5]={5, 10, 20, 200, 50};
 
-		int getOffset (int posX, int posY);
 };
